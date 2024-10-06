dbd43bafd60b27e66bec17be1256eddd71e10d06
WajahatShahzad <Wajji.shehzad@gmail.com>
2024-10-06 01:13:19 -0700

    latest

--- youtube-blog/sanity.cli.ts
+++ youtube-blog/sanity.cli.ts
@@ -5,9 +5,13 @@ export default defineCliConfig({
     projectId: '86l1kpea',
     dataset: 'production'
   },
+  project: {
+    basePath: '/studio'
+  },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
+  studioHost:'wajahat123$'
 })
